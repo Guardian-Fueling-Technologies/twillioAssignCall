6db01ebc5027e0689801dc54c413619f4bbfcc72
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2024-01-16 15:30:38 -0500

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -4,16 +4,15 @@ from twilio.rest import Client
 import os
 
 app = Flask(__name__)
-
+            
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
     """Respond to incoming phone calls with a menu of options"""
     # Start our TwiML response
     resp = VoiceResponse()
 
-    # Start our <Gather> verb
-    gather = Gather(num_digits=1, action='/gather')
-    gather.say('For sales, press 1. For support, press 2. For again press 3')
+    # If Twilio's request to our app included already gathered digits,
+    # process them
     if 'Digits' in request.values:
         # Get which digit the caller chose
         choice = request.values['Digits']
@@ -26,13 +25,17 @@ def voice():
             resp.say('You need support. We will help!')
             return str(resp)
         elif choice == '3':
-            resp.say('For sales, press 1. For support, press 2. For again press 3')
             resp.redirect('/voice')
         else:
             # If the caller didn't choose 1 or 2, apologize and ask them again
             resp.say("Sorry, I don't understand that choice.")
 
-    return str(resp)
+    # Start our <Gather> verb
+    gather = Gather(num_digits=1)
+    gather.say('For sales, press 1. For support, press 2.')
+    resp.append(gather)
 
+    return str(resp)
+        
 if __name__ == "__main__":
             app.run(port=8000, host='0.0.0.0', threaded=True)
