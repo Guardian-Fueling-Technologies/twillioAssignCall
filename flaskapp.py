c7302157dd30728b90b4cdf82040fa53ed599c57
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2024-01-26 11:09:58 -0500

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -23,38 +23,53 @@ SQLaddress = os.environ.get("addressGFT")
 account_sid = os.environ.get("account_sid")
 auth_token = os.environ.get("auth_token")
 
-app = Flask(__name__)
-def updateTwilio(status, message_timestamp, response_timestamp, ticket_no):
-    global twiliodf
+app = Flask(__name__)def updateTwilio(row, status, message_timestamp, response_timestamp, ticket_no):
     conn_str = f"DRIVER={SQLaddress};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
     
     update_query = f'''
-        UPDATE [GFT].[dbo].[MR_T_TwilioOnCall]
-        SET status = ?,
-            message_timestamp = ?,
-            response_timestamp = ?
-        WHERE ticket_no = ?
+        INSERT INTO [dbo].[MR_Report_TwilioOnCall] (
+            [text_Message],
+            [voice_Message],
+            [escalation_Message],
+            [ticket_no],
+            [Technician_ID],
+            [technician_NMBR],
+            [manager_NMBR],
+            [twilio_NMBR],
+            [status],
+            [message_timestamp],
+            [response_timestamp],
+            [calltime],
+            [callManagertime],
+            [LastUpdated]
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     '''
+    values = (
+        row.get('text_Message', ''),
+        row.get('voice_Message', ''), 
+        row.get('escalation_Message', ''),
+        ticket_no,
+        row.get('Technician_ID', ''),
+        row.get('technician_NMBR', ''),
+        row.get('manager_NMBR', ''),
+        row.get('twilio_NMBR', ''),
+        status,
+        message_timestamp,
+        response_timestamp,
+        row.get('calltime', ''),  # [calltime]
+        row.get('callManagertime', ''),  # [callManagertime]
+        datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")
+    )
     
-    values = (status, message_timestamp, response_timestamp, ticket_no)
     cursor.execute(update_query, values)
     conn.commit()
-
-    sql_query = '''
-    SELECT * FROM [GFT].[dbo].[MR_T_TwilioOnCall]
-    '''    
-
-    cursor.execute(sql_query)
-    columns = [column[0] for column in cursor.description]
-
-    result = cursor.fetchall()
-    data = [list(row) for row in result]
-    twiliodf = pd.DataFrame(data, columns=columns)
     cursor.close()
     conn.close()
 
+app = Flask(__name__)
 @app.route('/')
 def main_page():
     global twiliodf
@@ -63,7 +78,7 @@ def main_page():
     cursor = conn.cursor()
 
     sql_query = '''
-        SELECT * FROM [GFT].[dbo].[MR_T_TwilioOnCall]
+        SELECT * FROM [dbo].[MR_Staging_TwilioOnCall]
         '''    
 
     cursor.execute(sql_query)
@@ -74,8 +89,6 @@ def main_page():
     twiliodf = pd.DataFrame(data, columns=columns)
     table_html = twiliodf.to_html(classes='table table-bordered table-hover', index=False)
     return render_template('html/main.html', table_html=table_html)
-
-
 
 @app.route('/get_voice_response')
 def get_voice_response():
@@ -96,12 +109,12 @@ def update_rows():
 def assignCall(rows):
     global voice_response_str
     for index, row in twiliodf.iterrows():
-        assignQuestion = row['assign_Message']
-        tech_phone_number = row['technician_NMBR']
-        manager_NMBR = row['manager_NMBR']
-        twilio_number = row['twilio_NMBR']
-        firstdelaytime=1
-        callManagertime=2
+        assignQuestion = row.get('text_Message', '')
+        tech_phone_number = row.get('technician_NMBR', '')
+        manager_NMBR = row.get('manager_NMBR', '')
+        twilio_number = row.get('twilio_NMBR', '')
+        firstdelaytime = row.get('calltime', '')
+        callManagertime = row.get('callManagertime', '')
         client = Client(account_sid, auth_token)
         call_timestamps = []
         # message method
@@ -135,7 +148,7 @@ def assignCall(rows):
                         from_=twilio_number,
                         to=tech_phone_number
                     )
-                    updateTwilio(1, message_timestamp, latest_response.date_sent, row['ticket_no'])
+                    updateTwilio(row, 1, message_timestamp, latest_response.date_sent, row['ticket_no'])
                     break 
                 elif (
                     latest_response.body.lower() == no3CharWord
@@ -146,7 +159,7 @@ def assignCall(rows):
                         from_=twilio_number,
                         to=tech_phone_number
                     )
-                    updateTwilio(2, message_timestamp, latest_response.date_sent, row['ticket_no'])
+                    updateTwilio(row, 2, message_timestamp, latest_response.date_sent, row['ticket_no'])
                     break
             else:
                 print("never text before", datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
@@ -172,17 +185,18 @@ def assignCall(rows):
                     return 
                 if len(call_timestamps) == 0:
                     call_timestamps.append(datetime.now(timezone.utc))
+                    params = row.get('voice_Message', '')
+                    encoded_params = quote(params)
                     call = client.calls.create(
                         to=tech_phone_number,
                         from_="+18556258756",
-                        url="https://twilliocall.guardianfueltech.com/voice?callMessage=Guardiande%20default%20call%20message"
-                    )
+                        url = f"https://twilliocall.guardianfueltech.com/voice?callMessage={encoded_params}"
+                        )
                     print("Initiating a phone call to remind the tech to acknowledge the call.")
 
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
     # Start our TwiML response
-    resp = VoiceResponse()
     global voice_response_str
     callMessage = request.args.get('callMessage')
 
@@ -195,21 +209,26 @@ def voice():
 
         # <Say> a different message depending on the caller's choice
         if choice == '1':
+            resp = VoiceResponse()
             resp.say('You have accepted the call. Good for you!')
             voice_response_str = str(resp) 
             return "1"
         elif choice == '2':
+            resp = VoiceResponse()
             resp.say('You have declined the call. We will help!')
             voice_response_str = str(resp) 
             return "2"
         elif choice == '3':
+            resp = VoiceResponse()
             resp.say('You pressed replay voice ')
-            resp.redirect('/voice')
+            return redirect(f'/voice?callMessage={callMessage}')
         else:
-            # If the caller didn't choose 1 or 2, apologize and ask them again
-            resp.say("Sorry, I don't understand that choice.")
+            resp = VoiceResponse()
+            resp.say("I didn't get your response please Press 1 to Accept 2 to Decline")
+            resp.redirect('/voice')
 
-    gather = Gather(num_digits=1)
+    resp = VoiceResponse()
+    gather = Gather(timeout=5, num_digits=1)
     gather.say(f'{callMessage}To accept, press 1. To decline, press 2. To replay voice please press 3.')
     resp.append(gather)
     
