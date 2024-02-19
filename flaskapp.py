246fabeb99c702578829c2f2b5002dd45fd2729f
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2024-02-19 15:46:38 -0500

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -97,137 +97,110 @@ def get_voice_response():
     global voice_response_str
     return jsonify(response=voice_response_str)
 
-@app.route('/progress', methods=['GET', 'POST'])
-def update_rows():
-    threads = []
-    global twiliodf
-    print(twiliodf)
-    for index, row in twiliodf.iterrows():
-        assign_thread = threading.Thread(target=assignCall, args=(row,))
-        threads.append(assign_thread)
-        assign_thread.start()
+def assignCall(row):
+    global voice_response_str
+    assignQuestion = row.get('text_Message', '')
+    tech_phone_number = row.get('technician_NMBR', '')
+    twilio_number = row.get('twilio_NMBR', '')
+    firstdelaytime = row.get('calltime', '')
+    callManagertime = row.get('callManagertime', '')
+    ticket_no = row.get('ticket_no', '')
+    Max_Escalations = row.get('Max_Escalations', '')
+    escalation_Order = row.get('escalation_Order', '')
+    client = Client(account_sid, auth_token)
+    call_timestamps = []
+    # first time message method
+    print(Max_Escalations)
+    for localEscalation in range(0, Max_Escalations+1):
+        if localEscalation == 0:
+            yes3CharWord = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
+            message_timestamp = datetime.now(timezone.utc)
+            message = client.messages.create(
+                body=assignQuestion+f" If yes, please reply with '{yes3CharWord}'.",
+                from_=twilio_number,
+                to=tech_phone_number
+            )
+            while True:
+                # check every 10 sec
+                time.sleep(5)
+
+                response = client.messages.list(
+                    from_=tech_phone_number,
+                    to=twilio_number,
+                    limit=1
+                )
+                message_timestamp_str = message_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+                if response:
+                    print(voice_response_str, response[0].body, datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
+                    latest_response = response[0]
+                    if (voice_response_str == "1" or 
+                        latest_response.body.lower().strip() == yes3CharWord
+                        and latest_response.date_sent > message_timestamp
+                    ):
+                        message = client.messages.create(
+                            body=f" you have acknowledged the call {ticket_no}. Thank you",
+                            from_=twilio_number,
+                            to=tech_phone_number
+                        )
+                        updateReport(row, 1, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'])
+                        voice_response_str = "initial"
+                        break 
+                else:
+                    print("never text before", datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
+                # overtime
+                if datetime.now(timezone.utc) - message_timestamp > timedelta(minutes=firstdelaytime):
+                    if len(call_timestamps) != 0 and datetime.now(timezone.utc) - message_timestamp > timedelta(minutes=callManagertime):
+                        # text technician
+                        message = client.messages.create(
+                            body=f" We have elevate the call to your service manager due to overtime.",
+                            from_=twilio_number,
+                            to=tech_phone_number
+                        )
+                        updateProc(ticket_no, localEscalation+1)
+                        updateReport(row, 2, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'])
+
+                    # call repeat or datetime.now(timezone.utc) - call_timestamps[0] == timedelta(minutes=10)
+                        return 
+                    if len(call_timestamps) == 0:
+                        call_timestamps.append(datetime.now(timezone.utc))
+                        params = replace_numbers_with_spoken(row.get('voice_Message', ''))
+                        encoded_params = quote(params)
+                        call = client.calls.create(
+                            to=tech_phone_number,
+                            from_="+18556258756",
+                            url = f"https://twilliocall.guardianfueltech.com/voice/{ticket_no}?callMessage={encoded_params}"
+                            )
+                        voice_response_str = call._context
+                        print("Initiating a phone call to remind the tech to acknowledge the call.")
+                        
+
+@app.route('/progress/<ticket_no>', methods=['GET', 'POST'])
+def trigger_update_rows():
+    update_rows()
     return render_template('html/call.html')
 
-def assignCall(rows):
-    global voice_response_str
-    for index, row in twiliodf.iterrows():
-        assignQuestion = row.get('text_Message', '')
-        tech_phone_number = row.get('technician_NMBR', '')
-        manager_NMBR = row.get('manager_NMBR', '')
-        twilio_number = row.get('twilio_NMBR', '')
-        firstdelaytime = row.get('calltime', '')
-        callManagertime = row.get('callManagertime', '')
-        client = Client(account_sid, auth_token)
-        call_timestamps = []
-        # message method
-        yes3CharWord = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
-        no3CharWord = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
-        message_timestamp = datetime.now(timezone.utc)
-        message = client.messages.create(
-            body=assignQuestion+f" If yes, please reply with '{yes3CharWord}'. If not, reply with '{no3CharWord}'.",
-            from_=twilio_number,
-            to=tech_phone_number
-        )
-        while True:
-            # check every 10 sec
-            time.sleep(10)
-
-            response = client.messages.list(
-                from_=tech_phone_number,
-                to=twilio_number,
-                limit=1
-            )
-            message_timestamp_str = message_timestamp.strftime("%Y-%m-%d %H:%M:%S")
-            if response:
-                print(voice_response_str, response[0].body, datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
-                latest_response = response[0]
-                if (voice_response_str == "1" or 
-                    latest_response.body.lower() == yes3CharWord
-                    and latest_response.date_sent > message_timestamp
-                ):
-                    message = client.messages.create(
-                        body=f" you have accepted the call. Thank you",
-                        from_=twilio_number,
-                        to=tech_phone_number
-                    )
-                    updateTwilio(row, 1, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'])
-                    voice_response_str = "initial"
-                    break 
-                elif (voice_response_str == "2" or 
-                    latest_response.body.lower() == no3CharWord
-                    and latest_response.date_sent > message_timestamp
-                ):                
-                    message = client.messages.create(
-                        body=f" you have declined the call. Thank you",
-                        from_=twilio_number,
-                        to=tech_phone_number
-                    )
-                    updateTwilio(row, 2, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'])
-                    voice_response_str = "initial"
-                    break
-            else:
-                print("never text before", datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
-            # overtime
-            if datetime.now(timezone.utc) - message_timestamp > timedelta(minutes=firstdelaytime):
-                # print(datetime.now(timezone.utc) - message_timestamp)
-                if len(call_timestamps) != 0 and datetime.now(timezone.utc) - message_timestamp > timedelta(minutes=callManagertime):
-                    # text technician
-                    message = client.messages.create(
-                        body=f" We have elevate the call to your service manager due to overtime.",
-                        from_=twilio_number,
-                        to=tech_phone_number
-                    )
-                    print("call service manager")
-                    call_timestamps.append(datetime.now(timezone.utc))
-                    call = client.calls.create(
-                        to=manager_NMBR,
-                        from_="+18556258756",
-                        url=f"https://twilliocall.guardianfueltech.com/voice?callMessage={encoded_params}"
-                    )
-                    print("Initiating a phone call to elevate to acknowledge the call.")
-                # call repeat or datetime.now(timezone.utc) - call_timestamps[0] == timedelta(minutes=10)
-                    return 
-                if len(call_timestamps) == 0:
-                    call_timestamps.append(datetime.now(timezone.utc))
-                    params = row.get('voice_Message', '')
-                    encoded_params = quote(params)
-                    call = client.calls.create(
-                        to=tech_phone_number,
-                        from_="+18556258756",
-                        url = f"https://twilliocall.guardianfueltech.com/voice?callMessage={encoded_params}"
-                        )
-                    print("Initiating a phone call to remind the tech to acknowledge the call.")
-
-@app.route("/voice", methods=['GET', 'POST'])
+@app.route("/voice/<ticket_no>", methods=['GET', 'POST'])
 def voice():
     resp = VoiceResponse()
-    # Start our TwiML response
-    global voice_response_str
     callMessage = request.args.get('callMessage')
-
-
-    # If Twilio's request to our app included already gathered digits,
-    # process them
     if 'Digits' in request.values:
-        # Get which digit the caller chose
         choice = request.values['Digits']
 
-        # <Say> a different message depending on the caller's choice
         if choice == '1':
             resp.say('You have accepted the call. Good for you!')
             voice_response_str = "1"
-            return str(resp) 
+            return "1" 
         elif choice == '2':
             resp.say('You have declined the call. We will help!')
             voice_response_str = "2"
-            return str(resp) 
+            return "2" 
         else:
             resp.say('I did not get your response.')
 
     gather = Gather(timeout=5, num_digits=1)
     gather.say(f'{callMessage}To accept, press 1. To decline, press 2. To replay voice please press 3.')
     resp.append(gather)
-    resp.redirect(f'/voice?callMessage={callMessage}')
+    resp.redirect('/voice')
     return str(resp)
     
 if __name__ == "__main__":
