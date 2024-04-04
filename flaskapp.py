3ac2a4f11d9c3634a0ce6bca5a0bd71527b2ff8a
Charlie (YuanChi) Chung <YuanChiChung@guardianfueltech.com>
2024-04-04 11:59:01 -0400

    test version

--- flaskapp.py
+++ flaskapp.py
@@ -50,28 +50,31 @@ class serverFunct():
 
             sql_query = '''
             SELECT
-            [text_Message]
-            ,[voice_Message]
-            ,[Ack_Message]
-            ,[overTime_message]
-            ,[Max_Escalations]
-            ,[Processed]
-            ,[ticket_no]
-            ,[Technician_ID]
-            ,[technician_NMBR]
-            ,[manager_NMBR]
-            ,[twilio_NMBR]
-            ,[status]
-            ,[message_timestamp]
-            ,[response_timestamp]
-            ,[calltime]
-            ,[callManagertime]
-            ,[LastUpdated]
-            FROM [GFT].[dbo].[MR_Staging_TwilioOnCall] WITH(NOLOCK)
-            WHERE [technician_NMBR] <> ? AND Processed <> 1;
+						 [text_Message]
+						,[voice_Message]
+						,[Ack_Message]
+						,[overTime_message]
+						,[Max_Escalations]
+						,[Processed]
+						,[ticket_no]
+						,[Technician_ID]
+						,[technician_NMBR]
+						,[twilio_NMBR]
+						,[status]
+						,[message_timestamp]
+						,[response_timestamp]
+						,[calltime]
+						,[escalation_time]
+						,[LastUpdated]
+					FROM [MR_Staging_TwilioOnCall] WITH(NOLOCK)
+					WHERE Technician_ID = 'CAR426'
             '''
+            # origin
+            # FROM [GFT].[dbo].[MR_Staging_TwilioOnCall] WITH(NOLOCK)
+            # WHERE [technician_NMBR] <> ? AND Processed <> 1;
+            # cursor.execute(sql_query, "None")
 
-            cursor.execute(sql_query, "None")
+            cursor.execute(sql_query)
             columns = [column[0] for column in cursor.description]
 
             result = cursor.fetchall()
@@ -187,7 +190,7 @@ def assignCall(row):
     tech_phone_number = row.get('technician_NMBR', '')
     twilio_number = row.get('twilio_NMBR', '')
     firstdelaytime = row.get('calltime', '')
-    callManagertime = row.get('callManagertime', '')
+    escalation_time = row.get('escalation_time', '')
     ticket_no = row.get('ticket_no', '')
     Max_Escalations = row.get('Max_Escalations', '')
     client = Client(account_sid, auth_token)
@@ -213,10 +216,11 @@ def assignCall(row):
                     to=twilio_number,
                     limit=1
                 )
+                
                 message_timestamp_str = message_timestamp.strftime("%Y-%m-%d %H:%M:%S")
                 if response:
                     global responseArr
-                    print(responseArr[(int)(ticket_no.split("-")[1])], response[0].body, datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
+                    print((int)(ticket_no.split("-")[1]), responseArr[(int)(ticket_no.split("-")[1])], response[0].body, datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
                     latest_response = response[0] 
                     if (
                         latest_response.body.lower().strip() == yes3CharWord
@@ -235,7 +239,8 @@ def assignCall(row):
                     print("never text before", datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
                 # overtime
                 if datetime.now(timezone.utc) - message_timestamp > timedelta(minutes=firstdelaytime):
-                    if len(call_timestamps) != 0 and datetime.now(timezone.utc) - message_timestamp > timedelta(minutes=callManagertime):
+                    print(escalation_time)
+                    if len(call_timestamps) != 0 and datetime.now(timezone.utc) - message_timestamp > timedelta(minutes=escalation_time):
                         # text technician
                         message = client.messages.create(
                             body=f" We have elevate the call to your manager due to overtime.",
@@ -243,7 +248,7 @@ def assignCall(row):
                             to=tech_phone_number
                         )
                         localEscalation += 1
-                        serverFunct.updateReport(row, localEscalation, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'], localEscalation+1)
+                        serverFunct.updateReport(row, 2, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'], localEscalation+1)
                         break
                     # call repeat or datetime.now(timezone.utc) - call_timestamps[0] == timedelta(minutes=10)
                     if len(call_timestamps) == 0:
@@ -257,63 +262,69 @@ def assignCall(row):
                             )
         else:
             escalationData = serverFunct.updateProc(ticket_no, localEscalation)
-            # print(escalationData)
-            if(escalationData.Action=="Call"):
-                params = messageEditor.replace_numbers_with_spoken(escalationData.message)
-                encoded_params = quote(params)
-                call = client.calls.create(
-                    to=tech_phone_number,
-                    from_="+18556258756",
-                    url = f"https://twilliocall.guardianfueltech.com/voice/{ticket_no}?callMessage={encoded_params}"
+            if escalationData.Phone:
+                print(escalationData)
+                if(escalationData.Action=="Call"):
+                    params = messageEditor.replace_numbers_with_spoken(escalationData.message)
+                    encoded_params = quote(params)
+                    # print(encoded_params)
+                    call = client.calls.create(
+                        # to=escalationData.Phone,
+                        to=tech_phone_number,
+                        from_="+18556258756",
+                        url = f"https://twilliocall.guardianfueltech.com/voice/{ticket_no}?callMessage={encoded_params}"
+                        )
+                    time.sleep(60*2)
+                elif(escalationData.Action=="Message"):
+                    yes3CharWord = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
+                    message_timestamp = datetime.now(timezone.utc)
+                    message = client.messages.create(
+                        body=assignQuestion+f" If yes, please reply with '{yes3CharWord}'.",
+                        from_=twilio_number,
+                        to=escalationData.Phone
                     )
-                time.sleep(60*2)
-            elif(escalationData.Action=="Message"):
-                yes3CharWord = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
-                message_timestamp = datetime.now(timezone.utc)
-                message = client.messages.create(
-                    body=assignQuestion+f" If yes, please reply with '{yes3CharWord}'.",
-                    from_=twilio_number,
-                    to=tech_phone_number
-                )
-                while True:
-                    time.sleep(5)
-                    response = client.messages.list(
-                        from_=tech_phone_number,
-                        to=twilio_number,
-                        limit=1
-                    )
-                    message_timestamp_str = message_timestamp.strftime("%Y-%m-%d %H:%M:%S")
-                    if response:
-                        print(response[0].body, datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
-                        latest_response = response[0]
-                        # voice_response_str == "1" or 
-                    if (
-                        latest_response.body.lower().strip() == yes3CharWord
-                        and latest_response.date_sent > message_timestamp or responseArr[(int)(ticket_no.split("-")[1])] == 1
+                    while True:
+                        time.sleep(5)
+                        response = client.messages.list(
+                            from_=escalationData.Phone,
+                            to=twilio_number,
+                            limit=1
+                        )
+                        message_timestamp_str = message_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+                        if response:
+                            print(response[0].body, datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
+                            latest_response = response[0]
+                            # voice_response_str == "1" or 
+                        if (
+                            latest_response.body.lower().strip() == yes3CharWord
+                            and latest_response.date_sent > message_timestamp or responseArr[(int)(ticket_no.split("-")[1])] == 1
+                            ):
+                            message = client.messages.create(
+                                body=f" you have acknowledged the call {ticket_no}. Thank you",
+                                from_=twilio_number,
+                                to=escalationData.Phone
+                            )
+                            responseArr[(int)(ticket_no.split("-")[1])] = None
+                            serverFunct.updateReport(row, 2, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'], 0)
+                            # end of case
+                            return 
+                        else:
+                            print("never text before", datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
+                if (responseArr[(int)(ticket_no.split("-")[1])] == 1
                     ):
-                        message = client.messages.create(
-                            body=f" you have acknowledged the call {ticket_no}. Thank you",
-                            from_=twilio_number,
-                            to=tech_phone_number
-                        )
-                        responseArr[(int)(ticket_no.split("-")[1])] = None
-                        serverFunct.updateReport(row, localEscalation, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'], 0)
-                        # end of case
-                        return 
-                    else:
-                        print("never text before", datetime.now(timezone.utc) - message_timestamp, message_timestamp_str)
-            if (responseArr[(int)(ticket_no.split("-")[1])] == 1
-                ):
-                        message = client.messages.create(
-                            body=f" Please contact your manager!",
-                            from_=twilio_number,
-                            to=tech_phone_number
-                        )
-                        responseArr[(int)(ticket_no.split("-")[1])] = None
-                        serverFunct.updateReport(row, localEscalation, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'], 0)
-                        # end of case
-                        return 
-            localEscalation += 1
+                            message = client.messages.create(
+                                body=f" Please contact your manager!",
+                                from_=twilio_number,
+                                to=escalationData.Phone
+                            )
+                            responseArr[(int)(ticket_no.split("-")[1])] = None
+                            serverFunct.updateReport(row, 2, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'], 0)
+                            # end of case
+                            return 
+                localEscalation += 1
+            else:
+                serverFunct.updateReport(row, 3, message_timestamp, latest_response.date_sent.strftime("%Y-%m-%d %H:%M:%S"), row['ticket_no'], 0)
+                return
             
             
 # twilio customize voice 
@@ -343,8 +354,10 @@ def voice(ticket_no):
 if __name__ == "__main__":   
     serverFunct.unUpdateStaging()
     # ticketno sample 	
-    ticket_no = "240218-0020"
+    # ticket_no = "240218-0020"
     # serverFunct.updateProc(ticket_no, 1)
-    
     threading.Thread(target=serverFunct.getTwillioStaging, daemon=True).start()
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    # print(serverFunct.getTwillioStaging)
     app.run(port=8000, host='0.0.0.0', threaded=True)
